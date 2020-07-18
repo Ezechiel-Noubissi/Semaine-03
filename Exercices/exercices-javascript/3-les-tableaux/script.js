e118b9e759e040389440b2537718d2ec6a860f85
Ezéchiel Noubissi <ezchiele@siriusschool.be>
2020-07-18 14:05:47 +0200

    Update

--- Exercices/exercices-javascript/3-les-tableaux/script.js
+++ Exercices/exercices-javascript/3-les-tableaux/script.js
@@ -1,20 +1,33 @@
 // Exercice 1: ajoute "Ted" au tableau suivant
 
 let castArray= ["Robin", "Barney", "Lily", "Marshal"];
+castArray.push("Ted");
+const findIndexFunction = (array, arg) => {
+    return array.findIndex(element => element === arg)
+}
 
 // Exercice 2: Affiche l'index de "Ted" dans le tableau dans la console
+let tedIndex = findIndexFunction(castArray, "Ted");
+console.log(tedIndex)
+
 
 // Exercice 3: Affiche l'index de Robin dans le tableau dans la console
+let robinIndex = findIndexFunction(castArray, "Robin");
+console.log(robinIndex)
 
 // Exercice 4: retire "Robin" du tableau
+castArray.shift();
 
 // Exercice 5: retire "Ted" du tableau
+castArray.pop();
 
 // Exercice 6: ajoute "Ted" et "Robin" au tableau
+castArray.push("Ted", "Robin")
 
 // Exercice 7: Affiche le nouvel index de Ted et Robin dans le tableau dans la console
 
 // Exercice 8: à l'aide d'une boucle, affiche chaque élément du tableau dans la console
+castArray.forEach(element => console.log(element))
 
 // Exercice 9: à l'aide de la méthode .findIndex(), trouve l'index de Lily, ensuite, affiche Lily dans la console
 
